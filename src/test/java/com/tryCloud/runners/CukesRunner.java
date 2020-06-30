61832959ceb0e13067f2e906ce787bbb0694999e
celikyus01 <celikyus01@gmail.com>
2020-06-30 13:14:30 +0100

    add login.fature, navigate.feature

--- src/test/java/com/tryCloud/runners/CukesRunner.java
+++ src/test/java/com/tryCloud/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/tryCloud/step_definitions",
         dryRun = false,
-        tags = "@navigate and @login"
+        tags = "@navigate"
 
         )
 public class CukesRunner {
